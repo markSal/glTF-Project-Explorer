da3a6bfbbd9caaa6c5f05976bb125f7f92a28938
Recursion (Nola) <1972553+markSal@users.noreply.github.com>
2023-07-31 14:08:05 -0500

    Removed empty element

--- src/components/ProjectCardHeader.tsx
+++ src/components/ProjectCardHeader.tsx
@@ -8,21 +8,19 @@ const ProjectCardHeader: React.FC<IProjectCardHeader> = (props) => {
   const { project } = props;
 
   return (
-    <>
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-        <h1 className="m-0 text-2xl">
-          {project.link ? <a href={project.link}>{project.name}</a> : project.name}
-        </h1>
-        {project.key && project.key.trim() !== "" && process.env.REACT_APP_PROJECT_UPDATE_URL && (
-          <a
-            className="update-project btn btn-primary mt-2 sm:mt-0 sm:ml-4"
-            href={process.env.REACT_APP_PROJECT_UPDATE_URL + project.key}
-          >
-            Update Project
-          </a>
-        )}
-      </div>
-    </>
+    <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+      <h1 className="m-0 text-2xl">
+        {project.link ? <a href={project.link}>{project.name}</a> : project.name}
+      </h1>
+      {project.key && project.key.trim() !== "" && process.env.REACT_APP_PROJECT_UPDATE_URL && (
+        <a
+          className="update-project btn btn-primary mt-2 sm:mt-0 sm:ml-4"
+          href={process.env.REACT_APP_PROJECT_UPDATE_URL + project.key}
+        >
+          Update Project
+        </a>
+      )}
+    </div>
   );
 };
 
